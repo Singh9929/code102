eba0aa532ddd7caba7fe612c046ad60e76535603
Chaman Singh <56739541+Singh9929@users.noreply.github.com>
2019-10-19 00:56:31 +0530

    Update elif.py

--- elif.py
+++ elif.py
@@ -1,7 +1,11 @@
+name=input("Enter Your name:")
 age=int(input("Enter your age:"))
 if age==0:
 	print("You can not watch")
 elif 0<age<=10:
-	print("half")
+	print("half price")
+elif 10<age<=50:
+	print("full price")
+
 else:
-	print("Full price")		
+	print("Sponsered")		
